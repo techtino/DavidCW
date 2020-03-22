84860b7c4a553f10e2fc0895bac3bc08f1b8d66b
TechTino <w1682146@my.westminster.ac.uk>
2020-03-22 15:10:59 +0000

    Added password security

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -2,11 +2,16 @@ package client;
 
 import java.net.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
+
 /**
  *
  * @author techtino
@@ -30,7 +35,7 @@ public class Client {
     
     public void sendNumbers(int[] lotNumbers){        
         try {
-            for (int i = 0; i <=5;i++){ // for size of the array, write the numbers to the server
+            for (int i = 0; i < lotNumbers.length;i++){ // for size of the array, write the numbers to the server
                 out.writeInt(lotNumbers[i]);
             }
         }
@@ -77,19 +82,25 @@ public class Client {
         }
     }    
 
-    public void getNumbers(){
+    public void getNumbers(){     
         SpinnerNumberModel sModel1 = new SpinnerNumberModel(1, 1, 50, 1);SpinnerNumberModel sModel2 = new SpinnerNumberModel(2, 1, 50, 1);SpinnerNumberModel sModel3 = new SpinnerNumberModel(3, 1, 50, 1);SpinnerNumberModel sModel4 = new SpinnerNumberModel(4, 1, 50, 1);SpinnerNumberModel sModel5 = new SpinnerNumberModel(5, 1, 50, 1);SpinnerNumberModel sModel6 = new SpinnerNumberModel(6, 1, 50, 1); // Create models for jSpinners to use (number between 1-50)
-        
         JSpinner spinner1 = new JSpinner(sModel1);JSpinner spinner2 = new JSpinner(sModel2);JSpinner spinner3 = new JSpinner(sModel3);JSpinner spinner4 = new JSpinner(sModel4);JSpinner spinner5= new JSpinner(sModel5);JSpinner spinner6= new JSpinner(sModel6); // Create jSpinners for use within JOptionPane
-        
         Object[] message = { "Number 1:", spinner1,"Number 2:", spinner2,"Number 3:", spinner3,"Number 4:", spinner4,"Number 5:", spinner5,"Number 6",spinner6}; // Construct structure for JOptionPane
-        
         int option = JOptionPane.showOptionDialog(null, message, "Lottery! Enter 6 numbers", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null); // Display OptionDialog and get option from user (OK, cancel, close)
+        int lotNumber1 = (int)spinner1.getValue();int lotNumber2 = (int)spinner2.getValue();int lotNumber3 = (int)spinner3.getValue();int lotNumber4 = (int)spinner4.getValue();int lotNumber5 = (int)spinner5.getValue();int lotNumber6 = (int)spinner6.getValue();
+        int[] lotNumbers = {lotNumber1,lotNumber2,lotNumber3,lotNumber4,lotNumber5,lotNumber6}; // Array of numbers created from user entered numbers
+        
+        for (int i = 0; i < lotNumbers.length; i++) { // For the length of the array, check if the number is the same as another one in the array
+            for (int k = i + 1; k < lotNumbers.length; k++) {
+                if (lotNumbers[i] == lotNumbers[k]) {
+                    JOptionPane.showMessageDialog(null, "You entered the same value more than once");
+                    getNumbers();
+                }
+            }
+        }
+        
         switch (option) {
             case JOptionPane.OK_OPTION: // if user presses ok, get values from spinners and create array with them
-                int lotNumber1 = (int)spinner1.getValue();int lotNumber2 = (int)spinner2.getValue();int lotNumber3 = (int)spinner3.getValue();int lotNumber4 = (int)spinner4.getValue();int lotNumber5 = (int)spinner5.getValue();int lotNumber6 = (int)spinner6.getValue();
-                
-                int[] lotNumbers = {lotNumber1,lotNumber2,lotNumber3,lotNumber4,lotNumber5,lotNumber6};
                 sendNumbers(lotNumbers);
                 break;
             default: { // any other option closes connection and closes client
@@ -103,13 +114,44 @@ public class Client {
             }
         }
     }
+    
+    public void getPasswordAndCheckIfValid(){
+        
+        try {
+            JPasswordField passwordField = new JPasswordField(10);
+            int passwordDialog = JOptionPane.showConfirmDialog(null,passwordField,"Enter Pass (Default: lottery)",JOptionPane.OK_CANCEL_OPTION); // create confirm dialog for password entering (default password lottery)
+            switch (passwordDialog){ // break out and continue program if ok button pressed, else exit program
+                case JOptionPane.OK_OPTION:
+                    break;
+                default:
+                    System.exit(0);
+            }
+            
+            String password = passwordField.getText(); // gets password from user
+            MessageDigest digest = MessageDigest.getInstance("SHA-256"); // creates digest object for hashing
+            byte[] encodedhash = digest.digest(password.getBytes(StandardCharsets.UTF_8)); // encode password into byte version of hash
+            StringBuilder sb = new StringBuilder();
+            for (byte b : encodedhash) { // build a string with the bytes from encoding
+                sb.append(String.format("%02x", b));
+            }
+            
+            if (!"be7f94bb10c4be7b447f065e21b301853b84964b94d3bc03dc11732dee212d73".equals(sb.toString())){ // checks if password is = to hashed value of 'lottery', if it isn't then tell user to re-enter password
+                JOptionPane.showMessageDialog(null,"The password you entered was incorrect");
+                getPasswordAndCheckIfValid();
+            }
+        } 
+        catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     public static void main(String args []){
-            Client client = new Client();
-            client.startConnection();           
-            client.getAck();
-            client.getNumbers();
-            client.getResult();
-            System.exit(0);
-        } 
+        Client client = new Client();
+        client.getPasswordAndCheckIfValid();
+        client.startConnection();           
+        client.getAck();
+        client.getNumbers();
+        client.getResult();
+        System.exit(0);
     }
+}
